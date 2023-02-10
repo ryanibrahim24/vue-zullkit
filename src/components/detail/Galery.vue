eda49a374d0e66f93f028ad1814e42460cf4e3c7
ryanibrahim24 <ryanibrahim27@gmail.com>
2023-02-10 16:23:59 +0700

    update

--- src/components/detail/Galery.vue
+++ src/components/detail/Galery.vue
@@ -3,30 +3,29 @@ import { ref } from "vue";
 
 const props = defineProps({
   defaultImage: String,
-  galleries: Array
-})
+  galleries: Array,
+});
 
 const thumbnail = ref(props.defaultImage);
 
-function changeImage(image){
-  this.thumbnail = image
+function changeImage(image) {
+  this.thumbnail = image;
 }
-
 </script>
 
 <template>
   <section id="gallery">
-    <img
-      :src="thumbnail"
-      alt="default image"
-      class="w-full mt-6 rounded-2xl"
-    />
+    <img :src="thumbnail" alt="default image" class="w-full mt-6 rounded-2xl" />
     <div class="grid grid-cols-4 gap-4 mt-4">
-        <template v-for="galery in galleries" :key="galery.id">
-            <div @click="changeImage(galery.url)" class="overflow-hidden cursor-pointer rounded-2xl" :class="{'ring-2 ring-indigo-500' : thumbnail == galery.url}"> 
-                <img :src="galery.url" class="w-full" alt="" />
-            </div>
-        </template>
+      <template v-for="galery in galleries" :key="galery.id">
+        <div
+          @click="changeImage(galery.url)"
+          class="overflow-hidden cursor-pointer rounded-2xl"
+          :class="{ 'ring-2 ring-indigo-500': thumbnail == galery.url }"
+        >
+          <img :src="galery.url" class="w-full" alt="" />
+        </div>
+      </template>
     </div>
   </section>
 </template>
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const route = useRoute();
 const userStore = useUserStore();
-
+const isLoggedIn = computed(() => userStore.isLoggedIn)
 const user = computed(() => userStore.getUser);
 const item = ref(false);
 
@@ -102,13 +102,19 @@ onMounted(() => {
                 </ul>
               </div>
 
-              <div v-if="user.data">
+              <div v-if="isLoggedIn" :user="user.data">
                 <a v-if="user.data.subscription.length > 0" :href="item.file"
                   class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
                   Download Now
                 </a>
                 <RouterLink v-else
                   to="/pricing"
+                  class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
+                  Subscribe
+                </RouterLink>
+              </div>
+              <div v-if="isLoggedIn = !isLoggedIn">
+                <RouterLink to="/pricing"
                   class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
                   Subscribe
                 </RouterLink>
